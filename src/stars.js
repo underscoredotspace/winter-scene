8a4634760964b04a5b41c02ba8b9bd24d1b59ead
Colin Tindle <ampersand@underscore.space>
2017-12-20 09:25:38 +0000

    Split index up

---
+++ src/stars.js
@@ -0,0 +1,74 @@
+import React from 'react'
+const SVGNS = 'http://www.w3.org/2000/svg'
+const rnd = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+export default class Stars extends React.Component {
+    state = {stars: []}
+    
+    makeStars = () => {
+      this.width = this.node.clientWidth
+      this.height = this.node.clientHeight
+      this.starDensity = this.height * this.width / 7000
+      
+      let stars = []
+      
+      for (let ndx = 0; ndx < this.starDensity; ndx++) {
+        stars.push({
+          x: rnd(-30, this.width),
+          y: rnd(-30, this.height-30),
+          size: rnd(15, 30),
+          cycleTime: rnd(5, 20),
+          rotation: rnd(0, 359)
+        })
+      }
+      
+      this.setState({stars})
+    }
+  
+    componentDidMount = () => {
+       window.addEventListener("resize", this.makeStars)
+       this.makeStars()
+    }
+  
+    render = () =>(
+      <div className="star-field" ref={n=>(this.node=n)}>
+        {this.state.stars.map((star, ndx) => (
+          <Star {...star} key={`star-${ndx}`} />
+        ))}
+      </div>
+    )
+  }
+  
+  class Star extends React.Component {
+    constructor(props) {
+      super(props)
+      const s = props.size
+  
+      const starStyle = {
+        top: `${props.y}px`,
+        left: `${props.x}px`,
+        width: `${s}px`,
+        height: `${s}px`,
+        transform: `rotate(${props.rotation}deg)`,
+        animation: `twinkle ${props.cycleTime}s infinite`
+      }
+  
+      const points = `0,${(s / 3).toFixed(2)} 
+        ${s},${(s / 3).toFixed(2)} 
+        ${(s / 6).toFixed(2)},${s} 
+        ${(s / 2).toFixed(2)},0 
+        ${s - (s / 6).toFixed(2)},${s}`
+  
+      this.state = {
+        points,
+        starStyle,
+        vb: `0 0 ${this.props.size} ${this.props.size}`
+      }
+    }
+  
+    render = ()  => (
+      <svg className="star" xmlns={SVGNS} style={this.state.starStyle} viewBox={this.state.vb}>
+        <polygon className="star" xmlns={SVGNS} points={this.state.points} />
+      </svg>
+    )
+  }
---
+++ src/trees.js
@@ -0,0 +1,128 @@
+import React from 'react'
+
+const SVGNS = 'http://www.w3.org/2000/svg'
+const rnd = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+export default class PineTrees extends React.Component {
+    state = {trees:[]}
+  
+    componentDidMount = () => {
+      const maxTreeH = 250
+      const minTreeH = 100
+  
+      const treeCount = window.innerWidth / 100
+      const segment = window.innerWidth / treeCount
+      const minBottom = window.innerHeight * (2/3)
+      const maxBottom = window.innerHeight
+  
+      let trees = []
+      for (let tree=0; tree<(treeCount+1); tree++) {
+        const size = rnd(minTreeH,maxTreeH)
+        const left = rnd(segment*(tree-1), segment*tree)
+        const ratio = (size-minTreeH) / (maxTreeH-minTreeH)
+        const bottom = minBottom + (ratio * (maxBottom-minBottom))
+        const top = bottom-size
+        trees.push({left, top, size})
+      }
+      this.setState({trees})
+    }
+  
+    render = () => (
+      <div className="pineTrees">
+        {this.state.trees.map((tree, ndx) => (
+          <PineTree {...tree} key={`tree-${ndx}`} />
+        ))}
+      </div>
+    )
+  }
+  
+  class PineTree extends React.Component {
+    state = {lines:[], polylines:[], snowLines:[]}
+  
+    setSize = () => {
+      this.outer = this.node.getBoundingClientRect()
+      const viewBox = `0 0 ${this.outer.width} ${this.outer.height}`
+      this.setState({viewBox})
+    }
+  
+    getLine = (x1, y1, angle, length) => {
+      let {x2,y2} = this.lineEnd(x1, y1, angle, length)
+      return {x1,y1,x2,y2}
+    }
+  
+    lineEnd = (x1, y1, angle, length) => {
+      angle = angle * Math.PI / 180
+      const x2 = x1 + (length * Math.sin(angle))
+      const y2 = y1 + (length * Math.cos(angle))
+      return {x2, y2}
+    }
+  
+    componentDidMount = () => {
+      if (!this.node) {return}
+      this.setSize()
+      let lines = [], polylines = [], snowLines = []
+      
+      const trunkAngle = rnd(175,185)
+      const trunkLength = this.outer.height
+      const trunkStart = {x:this.outer.width/2, y:this.outer.height}
+  
+      lines.push({...this.getLine(trunkStart.x, trunkStart.y, trunkAngle, trunkLength), className: 'pine-trunk'})
+  
+      const branches = this.outer.height/5
+      const segment = trunkLength/branches
+  
+      for (let angle of [[100,120], [240,260]]) {
+        for (let branch = 3; branch < branches; branch++) {
+          const posOnTrunk = rnd(segment*branch, segment*(branch+1))
+          const branchStart = this.lineEnd(trunkStart.x, trunkStart.y, trunkAngle, posOnTrunk)
+          const branchAngle = rnd(angle[0], angle[1])
+          const branchLength = (this.outer.width - branchStart.x2) * (1-(branch/branches))
+  
+          const subBranches = branchLength/3
+          const subSegment = branchLength/subBranches
+          let polyline = ""
+          for (let subBranch = 0; subBranch < subBranches-1; subBranch++) {
+            const posOnBranch = rnd(subSegment*subBranch, subSegment*(subBranch+1))
+            const subBranchStart = this.lineEnd(branchStart.x2, branchStart.y2, branchAngle, posOnBranch)
+            const subBranchAngle = rnd(170,190)
+            const subBranchLength = this.outer.height / 20
+            const polySeg = {...this.getLine(subBranchStart.x2, subBranchStart.y2+(subBranchLength/2), subBranchAngle, subBranchLength)}
+            polyline += `${polySeg.x1},${polySeg.y1} ${polySeg.x2},${polySeg.y2} `
+            if (rnd(0,9)%3===0) {
+              snowLines.push(polySeg)
+            }
+          }
+          polylines.push(polyline)
+        }
+      }
+  
+      this.setState({lines,polylines,snowLines})
+    }
+    
+    render = () => {
+      const style = {
+        top: this.props.top, 
+        left: this.props.left,
+        width:`${(this.props.size * 200) / 250}px`, 
+        height: `${this.props.size}px`
+      }
+      
+      return (
+        <svg className="pine-tree" style={style} xmlns={SVGNS} viewBox={this.state.viewBox} ref={n=>this.node=n}>
+          {
+            this.state.lines.map((line, ndx) => (
+              <line className="pine-branch" {...line} key={`line-${ndx}`} />
+            ))
+          }{
+            this.state.polylines.map((polyline, ndx) => (
+              <polyline className="pine-needle" points={polyline}  key={`polyline-${ndx}`} />
+            ))
+          }{
+            this.state.snowLines.map((line, ndx) => (
+              <line className="snow-needle" {...line} key={`snowline-${ndx}`} />
+            ))
+          }
+        </svg>
+      )
+    }
+  }
